6e1dd5ede3c6ac440076fadf26f94db23e331233
ElizabethRom <114512754+ElizabethRom@users.noreply.github.com>
2022-10-26 18:48:54 +0300

    Add files via upload

---
+++ dz8/script.js
@@ -0,0 +1,47 @@
+// let number;
+// function getNum(number){
+//     for(let i=1; i<=10; i++){
+//          number = +prompt('Введите число','');
+//         if(number>0){
+//             alert(number) 
+//         }
+//     }
+// };
+// getNum(number);
+
+// let number;
+// function getNum(number){
+//     for(let i=1; i<=10; i++){
+//          number = +prompt('Введите число','');
+//         if(number%2==0){
+//             alert(number) 
+//         }
+//     }
+// };
+// getNum(number);
+
+// let number;
+// function getNum(number){
+//     for(let i=1; i<=10; i++){
+//          number = +prompt('Введите число','');
+//         if(number%10==0){
+//             alert(number) 
+//         }
+//     }
+// };
+// getNum(number);
+
+let number;
+function getNum(number){
+    for(let i=1; i<=10; i++){
+         number = +prompt('Введите число','');
+    }
+};
+
+function showNum(number){
+    alert(number)
+};
+
+    if(getNum(number)>0){
+        showNum(number)
+    }
